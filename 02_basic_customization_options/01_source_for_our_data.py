b486e891d0e3edc8b55530dfa242054d885e821b
Matt Heerspink <mheerspink75@gmail.com>
2019-11-05 10:58:30 -0600

    updated 01_source_for_our_data.py

--- 02_basic_customization_options/01_source_for_our_data.py
+++ 02_basic_customization_options/01_source_for_our_data.py
@@ -1,15 +1,16 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import urllib.request
-import numpy as np 
+import numpy as np
 
-# https://cloud.iexapis.com/stable/stock/aapl/chart/5y?token=pk_553865f4628a441bbe94785fe772e5d5
 
 def graph_data(stock):
     print('Currently pulling: ', stock)
-    url = 'https://cloud.iexapis.com/stable/stock/' + stock + '/chart/5y?token=pk_553865f4628a441bbe94785fe772e5d5&format=csv'
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' + \
+        stock + '&interval=5min&apikey=K6QCETUG7BJZW11N&datatype=csv'
     source_code = urllib.request.urlopen(url).read().decode()
     print(source_code)
+
 
 stock = input('Stock to plot: ')
 graph_data(stock)
