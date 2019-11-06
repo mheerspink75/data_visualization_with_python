06cf0e4143ceaa4bc54d32a8d20b1e8adec3d09c
Matt Heerspink <mheerspink75@gmail.com>
2019-11-06 12:35:01 -0600

    updated 03_plotting_basic_stock_data.py

---
+++ 02_basic_customization_options/03_plotting_basic_stock_data.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import urllib.request
+import numpy as np
+import datetime as dt
+
+
+def bytespdate2num(fmt, encoding='utf-8'):
+    str_converter = mdates.datestr2num(fmt)
+    def bytes_converter(b):
+        s = b.decode(encoding)
+        return str_converter(s)
+    return bytes_converter
+
+
+def graph_data(stock):
+    print('Currently pulling: ', stock)
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' + \
+        stock + '&interval=5min&apikey=K6QCETUG7BJZW11N&datatype=csv'
+    source_code = urllib.request.urlopen(url).read().decode()
+    stock_data = []
+    split_source = source_code.split('\n')
+
+    for each_line in split_source:
+        split_line = each_line.split(',')
+        if len(split_line) == 6:
+            if 'values' not in each_line:
+                stock_data.append(each_line)
+
+    date, open, high, low, close, volume = np.loadtxt(stock_data, delimiter=',', unpack=True)
+
+    date_conv = np.vectorize(dt.datetime.fromtimestamp)
+    date =date_conv(date)
+
+    plt.plot_date(date, close, '-')
+    plt.show()
+
+
+stock = input('Stock to plot: ')
+graph_data(stock)
